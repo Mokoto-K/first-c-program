0b074b62c38dd775e3e30097fe63cb913a521bce
Mokoto-K <mokotok@protonmail.com>
2024-11-07 20:37:24 +1000

    Add files via upload

--- ex1-8.c
+++ ex1-8.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 
-int main() {
-    long nl, c;
-    
-    while (c = getchar() != EOF)
-	++nl;
-    printf("%ld\n", nl);
+int main () {
+	int c, nl, nb, nt;
+
+	nl = 0;
+	nb = 0;
+	nt = 0;
+
+	while ((c = getchar()) != EOF ) {
+
+            if (c == ' ') {
+		++nb;
+	    }
+
+	    if (c == '\t') {
+    		++nt;
+	    }
+
+	    if (c == '\n') {
+		++nl;
+	    }
+	}
+	printf("Total lines: %d\nTotal tabs: %d\nTotal spaces: %d\n", nl, nt, nb);
 }
 
